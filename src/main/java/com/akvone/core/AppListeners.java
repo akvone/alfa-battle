b1f654bce46aa3397b0c834d6383263a99a5b5d5
Kirill Vialkov <akvone@mail.ru>
2020-06-27 00:54:37 +0300

    Setup for postgre

--- src/main/java/com/akvone/core/AppListeners.java
+++ src/main/java/com/akvone/core/AppListeners.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,25 +11,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AppListeners {
 
-  private final KafkaTemplate<String, String> kafkaTemplate;
   private final MainRepository mainRepository;
 
   @EventListener(ApplicationReadyEvent.class)
   public void handle() {
-    genarateAndAddEntityToDB();
-    produceMessage();
-  }
-
-  private void produceMessage() {
-    var message = "AAA";
-    log.info(String.format("#### -> Producing message -> %s", message));
-    this.kafkaTemplate.send("users", message);
+//    genarateAndAddEntityToDB();
   }
 
   private void genarateAndAddEntityToDB() {
-    var e = new MainEntity();
-    e.setData("DATA");
-    mainRepository.save(e);
+//    var e = new MainEntity();
+//    e.setData("DATA");
+//    mainRepository.save(e);
   }
 
 }
