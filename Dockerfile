da549faf970fda0add67c30957e9cbd81ca820cb
Kirill Vialkov <akvone@mail.ru>
2020-06-26 21:31:18 +0300

    Modify docker start scripts

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM openjdk:11
 
 ADD target/alfa-battle-*.jar /app/app.jar
 COPY scripts/* /bin/
+RUN chmod +x /bin/*.sh
 
 EXPOSE 8080
 
--- start-with-docker.sh
+++ start-with-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mvn clean package
 docker build -t alfa-battle .
-docker run -p 8080:8080 alfa-battle
+docker run -it -p 8080:8080 alfa-battle
