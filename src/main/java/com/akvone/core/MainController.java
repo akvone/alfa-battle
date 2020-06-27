c3ced4511454f7f9844c936e53573eb64c1db34c
Kirill Vialkov <akvone@mail.ru>
2020-06-27 17:00:18 +0300

    Finally refactor

--- src/main/java/com/akvone/core/MainController.java
+++ src/main/java/com/akvone/core/MainController.java
@@ -1,19 +1,25 @@
-package com.akvone.core;
+package com.akvone.core.controller;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/main")
 @RequiredArgsConstructor
 @Slf4j
 public class MainController {
 
-  @GetMapping
-  public MainDTO get() {
-    return new MainDTO("/v1/main GET Result");
+
+  @GetMapping("atms/{id}")
+  public ResponseEntity<Map<String, String>> get(@PathVariable int id) {
+
+    return new ResponseEntity<>(Map.of("status", "atm not found"), HttpStatus.NOT_FOUND);
   }
+
+
 }
