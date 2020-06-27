37f8e247d89274462bb5539e60a73bfd3ccc8e3b
Kirill Vialkov <akvone@mail.ru>
2020-06-27 16:15:38 +0300

    Check

--- src/main/java/com/akvone/core/KafkaListeners.java
+++ src/main/java/com/akvone/core/KafkaListeners.java
@@ -19,7 +19,7 @@ public class KafkaListeners {
 
   @KafkaListener(
       topics = "RAW_PAYMENTS",
-      groupId = "akvone30",
+      groupId = "akvone40",
       properties = "auto.offset.reset=earliest"
   )
   public void handle(ConsumerRecord<String, String> record) {
